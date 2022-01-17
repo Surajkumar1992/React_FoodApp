f67c90b61b6d30f1b71e70cd4306c23e8aa69caf
SurajKumar1992 <suraj.subham@gmail.com>
2022-01-17 13:16:49 +0530

    Added Context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCartButton.module.css';
-
+import CartContext from "../../store/cart-context";
+import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
-    return <button className={classes.button} onClick={props.onClick}>
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>3</span>
+  const cartCtx = useContext(CartContext);
+
+//   const cartItemNo = cartCtx.items.reduce((curNo, item) => {
+//     return curNo + item.amount;
+//   }, 0);
+    const cartItemNo = cartCtx.items.reduce((curNo, item) => {
+            return curNo + item.amount;
+           }, 0);
+
+
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{cartItemNo}</span>
     </button>
+  );
 };
 
 export default HeaderCartButton;
